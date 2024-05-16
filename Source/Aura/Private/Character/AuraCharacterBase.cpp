0f6595fc38be1c37da9306261808814b54edc8d6
OctavioDiazG <odiazg01@outlook.com>
2024-05-16 12:53:14 -0600

    Character Blueprint Setup

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -7,6 +7,10 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon")); // Create a new skeletal mesh component
+	Weapon->SetupAttachment(GetMesh(), TEXT("WeaponHandSocket")); // Attach the weapon to the mesh in the WeaponHandSocket Name
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision); // Disable collision of the weapon Component
+
 }
 
 void AAuraCharacterBase::BeginPlay()
--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -17,4 +17,9 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, Category = "Combat") // 
+	TObjectPtr<USkeletalMeshComponent> Weapon;
+
+	
+
 };
