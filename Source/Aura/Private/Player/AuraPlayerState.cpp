9a3bed1876917974f8174c1fb73d5993bac8ad29
OctavioDiazG <odiazg01@outlook.com>
2024-05-18 16:38:31 -0600

    Add the Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+};
