b2ec807c9e8b967bb7a44c3dcd883e558b261f98
OctavioDiazG <odiazg01@outlook.com>
2024-06-10 15:50:44 -0600

    add AuraHUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UAuraUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget-> AddToViewport();
+}
---
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+class UAuraUserWidget;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
+};
