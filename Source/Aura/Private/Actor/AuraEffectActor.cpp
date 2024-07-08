41faebcf1029e4c20a4da50592f7d5182bebdcea
OctavioDiazG <odiazg01@outlook.com>
2024-07-08 10:05:46 -0600

    Duration Gameplay Effects
    Add Comments and documentation

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -11,7 +11,7 @@ AAuraEffectActor::AAuraEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot")); // Create a scene root component
 }
 
 void AAuraEffectActor::BeginPlay()
@@ -23,15 +23,16 @@ void AAuraEffectActor::BeginPlay()
 
 void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
+	// Get the ability system component of the target actor, if the Actor does not implement the UAbilitySystemComponent, this same function will look for a component in the actor that has a UAbilitySystemComponent
 	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
 
-	if (TargetASC == nullptr) return;
+	if (TargetASC == nullptr) return; // If the target actor doesn't have an ability system component, return
 
-	check(GameplayEffectClass);
-	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
-	EffectContextHandle.AddSourceObject(this);
-	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
-	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+	check(GameplayEffectClass); // Check if the GameplayEffectClass is valid, if the actor does not have a valid UAbilitySystemComponent, the engine will crash
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext(); // Create an effect context handle
+	EffectContextHandle.AddSourceObject(this);  // Add the source object to the effect context handle
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle); // Create a gameplay effect spec handle
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get()); // Apply the gameplay effect to the target actor, need to pass the data of the effect spec handle. the * is used to dereference the data of the effect spec handle because the ApplyGameplayEffectSpecToSelf function expects a FGameplayEffectSpecHandle object, not a FGameplayEffectSpecHandle pointer
 }
 
 
--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -15,16 +15,20 @@ class AURA_API AAuraEffectActor : public AActor
 
 public:
 	AAuraEffectActor();
+	//Set the component of the actor via Blueprints, that way its more flexible, and that can be done by a TechArtist or a Designer
 
 
 protected:
 	virtual void BeginPlay() override;
 	
 	UFUNCTION(BlueprintCallable)
-	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass); // function to apply a gameplay effect to a target actor.
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
-	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass; // class for an instant gameplay effect that we'd like to apply.
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass; // class for a duration gameplay effect that we'd like to apply.
 
 
 private:
